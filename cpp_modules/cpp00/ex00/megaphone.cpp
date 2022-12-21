0f6ce05289fcf45b6039114b2cba74a5b866787d
강지호 <gangjiho@gangjihoui-MacBookPro.local>
2022-12-21 17:34:45 +0900

    ex00/

--- cpp_modules/cpp00/ex00/megaphone.cpp
+++ cpp_modules/cpp00/ex00/megaphone.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <cstring>
 
-int main()
+void	printDefault() {
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+}
+
+void	printUpper(std::string str) {
+	for (unsigned long i = 0; i < str.length(); ++i) {
+		str[i] = toupper(str[i]);
+	}
+	std::cout << str;
+}
+
+int main(int ac, char **av)
 {
-
+	if (ac != 1) {
+		for (int i = 1; i < ac; ++i) {
+			printUpper(av[i]);
+		}
+	} else {
+		printDefault();
+	}
 	return (0);
 }
