61d49c14257096cfe99b9f7ae8f837478e912099
강지호 <gangjiho@gangjihoui-MacBookPro.local>
2022-12-26 14:45:26 +0900

    dp

--- algo/boj/silver/1912.cpp
+++ algo/boj/silver/1912.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int arr[100001];
-int dp[100001];
 int max_ = -22222222;
 
 int main() 
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 	int n;
-
-	cin >> n;
-	for (int i = 0; i < n; ++i) {
+    cin >> n;
+    vector<int> arr(n + 1);
+    vector<int> dp(n + 1);
+	for (int i = 1; i <= n; ++i) {
 		cin >> arr[i];
-	}
-	dp[0] = arr[0];
-	for (int i = 1; i < n; ++i) {
-		dp[i] = dp[i - 1] + arr[i];
-		if (dp[i] < arr[i])
-			dp[i] = arr[i];
-	}
-	for (int i = 0; i < n; ++i) {
-		if (dp[i] > max_) {
-			max_ = dp[i];
-		}
+		dp[i] = max(dp[i - 1] + arr[i], arr[i]);
+        max_ = max(max_, dp[i]);
 	}
 	cout << max_ << "\n";
 	return 0;
